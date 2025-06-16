d584d6f5155a062252ae43e5da9aadcf50b80e3b
Arnau Cebrian I Ortega <arcebria@car13s2.42barcelona.com>
2025-06-16 19:37:12 +0200

    seguimos con cpp04

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -1,41 +1,68 @@
 #include "Character.hpp"
 
 Character::Character(void) : _name("Unknown") {
-    for (int i = 0; i < 4; i++)
-        _inventory[i] = NULL;
-    std::cout << "Character default construcor called" << std::endl;
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = NULL;
+	std::cout << "Character default construcor called" << std::endl;
 }
 
 Character::Character(std::string const& name) : _name(name) {
-    for (int i = 0; i < 4; i++)
-        _inventory[i] = NULL;
-    std::cout << "Character constructor with string called" << std::endl;
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = NULL;
+	std::cout << "Character constructor with string called" << std::endl;
 }
 
 Character::Character(const Character& other) : _name(other._name) {
-    for (int i = 0; i < 4; i++) {
-        if (other._inventory[i])
-            _inventory[i] = other._inventory[i]->clone(); // creamos una nueva materia, no igualamos los punteros
-        else _inventory[i] = NULL;
-    }
-    std::cout << "Character copy constructor called" << std::endl;
+	for (int i = 0; i < 4; i++) {
+		if (other._inventory[i])
+			_inventory[i] = other._inventory[i]->clone(); // creamos una nueva materia, no igualamos los punteros
+		else _inventory[i] = NULL;
+	}
+	std::cout << "Character copy constructor called" << std::endl;
 }
 
 Character& Character::operator=(const Character& other) {
-    std::cout << "Character copy assignment operator called" << std::endl;
-    if (this != &other) {
-        for (int i = 0; i < 4; i++)
-            delete _inventory[i];
-        _name = other._name;
-        for (int i = 0; i < 4; i++) {
-            if (other._inventory[i])
-                _inventory[i] = other._inventory[i]->clone();
-            else _inventory[i] = NULL;
-        }
-    }
-    return *this;
+	std::cout << "Character copy assignment operator called" << std::endl;
+	if (this != &other) {
+		for (int i = 0; i < 4; i++)
+			delete _inventory[i];
+		_name = other._name;
+		for (int i = 0; i < 4; i++) {
+			if (other._inventory[i])
+				_inventory[i] = other._inventory[i]->clone();
+			else _inventory[i] = NULL;
+		}
+	}
+	return *this;
+}
+
+Character::~Character(void) {
+	for (int i = 0; i < 4; i++)
+		if (_inventory[i]) delete _inventory[i];
+	std::cout << "Character destructor called" << std::endl;
 }
 
 std::string const& Character::getName(void) const {
-    return _name;
+	return _name;
+}
+
+void	Character::equip(AMateria* m) {
+	for (int i = 0; i < 4; i++) {
+		if (_inventory[i] == NULL) {
+			_inventory[i] = m;
+			return;
+		}
+	}
+}
+
+void	Character::unequip(int idx) {
+	if (idx >= 0 && idx < 4)
+		_inventory[idx] = NULL;
+}
+
+void	Character::use(int idx, ICharacter& target) {
+	if (idx >= 0 && idx < 4 && _inventory[idx])
+		_inventory[idx]->use(target);
+	else
+		std::cout << "There's no materia equiped in this slot" << std::endl;
 }
---
+++ cpp04/ex03/MateriaSource.cpp
@@ -0,0 +1,56 @@
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource(void) {
+	for (int i = 0; i < 4; i++)
+		_templates[i] = NULL;
+	std::cout << "MateriaSource constructor called" << std::endl;
+}
+
+MateriaSource::MateriaSource(const MateriaSource& other) {
+	for (int i = 0; i < 4; i++) {
+		if (other._templates[i])
+			_templates[i] = other._templates[i]->clone();
+		else _templates[i] = NULL;
+	}
+	std::cout << "MaterialSource copy constructor called" << std::endl;
+}
+
+MateriaSource& MateriaSource::operator=(const MateriaSource& other) {
+	std::cout << "MateriaSource copy assignment operator called" << std::endl;
+	if (this != &other) {
+		for (int i = 0; i < 4; i++) {
+			if (_templates[i])
+				delete _templates[i];
+		}
+		for (int i = 0; i < 4; i++) {
+			if (other._templates[i])
+				_templates[i] = other._templates[i]->clone();
+			else _templates[i] = NULL;
+		}
+	}
+	return *this;
+}
+
+MateriaSource::~MateriaSource(void) {
+	for (int i = 0; i < 4; i++)
+		if (_templates[i]) delete _templates[i];
+}
+
+void MateriaSource::learnMateria(AMateria* m) {
+	if (!m)
+		return ;
+	for (int i = 0; i < 4; i++) {
+		if (_templates[i] == NULL) {
+			_templates[i] = m->clone();
+			return ;
+		}
+	}
+}
+
+AMateria* MateriaSource::createMateria(std::string const& type) {
+	for (int i = 0; i < 4; i++) {
+		if (_templates[i] && _templates[i]->getType() == type)
+			return _templates[i]->clone();
+	}
+	return NULL;
+}
