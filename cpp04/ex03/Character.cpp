cdb77c16879125ceaaf23742bcd28efdeb4cb708
BlasDeLezoEsGod <arnaucebrianortega@gmail.com>
2025-06-16 13:12:54 +0200

    full materiasss

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -1,9 +1,41 @@
 #include "Character.hpp"
 
 Character::Character(void) : _name("Unknown") {
+    for (int i = 0; i < 4; i++)
+        _inventory[i] = NULL;
     std::cout << "Character default construcor called" << std::endl;
 }
 
 Character::Character(std::string const& name) : _name(name) {
+    for (int i = 0; i < 4; i++)
+        _inventory[i] = NULL;
     std::cout << "Character constructor with string called" << std::endl;
+}
+
+Character::Character(const Character& other) : _name(other._name) {
+    for (int i = 0; i < 4; i++) {
+        if (other._inventory[i])
+            _inventory[i] = other._inventory[i]->clone(); // creamos una nueva materia, no igualamos los punteros
+        else _inventory[i] = NULL;
+    }
+    std::cout << "Character copy constructor called" << std::endl;
+}
+
+Character& Character::operator=(const Character& other) {
+    std::cout << "Character copy assignment operator called" << std::endl;
+    if (this != &other) {
+        for (int i = 0; i < 4; i++)
+            delete _inventory[i];
+        _name = other._name;
+        for (int i = 0; i < 4; i++) {
+            if (other._inventory[i])
+                _inventory[i] = other._inventory[i]->clone();
+            else _inventory[i] = NULL;
+        }
+    }
+    return *this;
+}
+
+std::string const& Character::getName(void) const {
+    return _name;
 }
--- cpp04/ex03/ICharacter.hpp
+++ cpp04/ex03/ICharacter.hpp
@@ -3,6 +3,7 @@
 
 # include <string>
 # include <iostream>
+# include "AMateria.hpp"
 
 class AMateria;
 
